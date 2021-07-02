40d3219b1ea065f0bbab793753c33527873e1106
pilagod <pilagooood@gmail.com>
2021-07-02 12:34:58 +0800

    refine withdraw messages

--- scripts/withdraw.ts
+++ scripts/withdraw.ts
@@ -1,25 +1,19 @@
 import { ethers } from "ethers";
 import { arbSigner, ethSigner, getBridge } from "../networks";
-import { printEventLog } from "../printer";
+import { printBalance, printEventLog } from "../printer";
 
 async function main() {
   const bridge = await getBridge();
   console.log(
     `Withdraw 0.1 ETH from Arb ${arbSigner.address} to Rin ${ethSigner.address}`
   );
-  console.log(
-    `Arb ETH balance before withdraw: ${ethers.utils.formatUnits(
-      await arbSigner.getBalance()
-    )}`
-  );
+  const beforeBalance = await arbSigner.getBalance();
   const tx = await bridge.withdrawETH(ethers.utils.parseEther("0.1"));
   const receipt = await tx.wait();
+  const afterBalance = await arbSigner.getBalance();
   console.log(`Events: `, (receipt.events ?? []).map(printEventLog));
-  console.log(
-    `Arb ETH balance after withdraw: ${ethers.utils.formatUnits(
-      await arbSigner.getBalance()
-    )}`
-  );
+  console.log(`Arb ETH balance before withdraw: ${printBalance(beforeBalance)}`);
+  console.log(`Arb ETH balance after withdraw: ${printBalance(afterBalance)}`);
   console.log(`Withdraw requested`);
 }
 
--- scripts/withdrawERC20.ts
+++ scripts/withdrawERC20.ts
@@ -1,29 +1,23 @@
 import { ethers } from "ethers";
 import { arbTestToken, ethTestToken } from "../contracts";
 import { arbSigner, ethSigner, getBridge } from "../networks";
-import { printEventLog } from "../printer";
+import { printBalance, printEventLog } from "../printer";
 
 async function main() {
   const bridge = await getBridge();
   console.log(
     `Withdraw 0.1 TKN from Arb ${arbSigner.address} to Rin ${ethSigner.address}`
   );
-  console.log(
-    `Arb TKN balance before withdraw: ${ethers.utils.formatUnits(
-      await arbTestToken.balanceOf(arbSigner.address)
-    )}`
-  );
+  const beforeBalance = await arbTestToken.balanceOf(arbSigner.address);
   const tx = await bridge.withdrawERC20(
     ethTestToken.address,
     ethers.utils.parseEther("0.1")
   );
   const receipt = await tx.wait();
+  const afterBalance = await arbTestToken.balanceOf(arbSigner.address);
   console.log("Events: ", (receipt.events ?? []).map(printEventLog));
-  console.log(
-    `Arb TKN balance after withdraw: ${ethers.utils.formatUnits(
-      await arbTestToken.balanceOf(arbSigner.address)
-    )}`
-  );
+  console.log(`Arb TKN balance before withdraw: ${printBalance(beforeBalance)}`);
+  console.log(`Arb TKN balance after withdraw: ${printBalance(afterBalance)}`);
   console.log(`Withdraw requested`);
 }
 
