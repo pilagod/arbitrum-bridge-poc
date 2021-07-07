805a40f09abee373ad8158cb1dfaf237ad5705f0
pilagod <pilagooood@gmail.com>
2021-07-07 18:07:51 +0800

    add L1Message model and repository

---
+++ worker/model/L1Message.ts
@@ -0,0 +1,25 @@
+export enum L1MessageStatus {
+  Sent = "Sent",
+  Retryable = "Retryable",
+  Done = "Done",
+}
+
+export class L1Message {
+  public id: number;
+  public createdAt: Date;
+
+  public constructor(
+    public msgId: number,
+    public msgData: string,
+    public l2TicketId: string,
+    public status: L1MessageStatus,
+    optionals: {
+      id?: number;
+      createdAt?: Date;
+    } = {}
+  ) {
+    const { id = 0, createdAt = new Date() } = optionals;
+    this.id = id;
+    this.createdAt = createdAt;
+  }
+}
---
+++ worker/repository/L1MessageRepository.ts
@@ -0,0 +1,64 @@
+import db from "../db";
+import { L1Message, L1MessageStatus } from "../model/L1Message";
+
+type L1MessageQuery = {
+  status?: L1MessageStatus;
+};
+
+export default class L1MessageRepository {
+  public findMany(query: L1MessageQuery): Promise<L1Message[]> {
+    return new Promise((resolve, reject) => {
+      const msgs: L1Message[] = [];
+      const conditions: string[] = [];
+      const conditionParams: any[] = [];
+      if (query.status) {
+        conditions.push(`status = ?`);
+        conditionParams.push(L1MessageStatus[query.status]);
+      }
+      let sql = "SELECT * FROM l1_message";
+      if (conditions.length > 0) {
+        sql += ` WHERE ${conditions.join(" AND ")}`;
+      }
+      db.each(
+        sql,
+        conditionParams,
+        (_, row) =>
+          msgs.push(
+            new L1Message(
+              row.msg_id,
+              row.msg_data,
+              row.l2_ticket_id,
+              row.status as L1MessageStatus,
+              {
+                id: row.id,
+                createdAt: new Date(row.created_at),
+              }
+            )
+          ),
+        (err) => (err ? reject(err) : resolve(msgs))
+      );
+    });
+  }
+
+  public create(msg: L1Message): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const sql =
+        "INSERT INTO l1_message (msg_id, msg_data, l2_ticket_id, status) VALUES (?, ?, ?, ?)";
+      db.run(sql, [msg.msgId, msg.msgData, msg.l2TicketId, msg.status], (err) =>
+        err ? reject(err) : resolve()
+      );
+    });
+  }
+
+  public update(msg: L1Message): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const sql =
+        "UPDATE l1_message SET msg_id = ?, msg_data = ?, l2_ticket_id = ?, status = ? WHERE id = ?";
+      db.run(
+        sql,
+        [msg.msgId, msg.msgData, msg.l2TicketId, msg.status, msg.id],
+        (err) => (err ? reject(err) : resolve())
+      );
+    });
+  }
+}
