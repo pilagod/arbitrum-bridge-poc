2d03a77e0f2be8ac1a0b4c6001796172a4a4063f
pilagod <pilagooood@gmail.com>
2021-07-07 10:41:07 +0800

    update migrations

--- migrations/sqls/20210706030905-create-l2-message-table-up.sql
+++ migrations/sqls/20210706030905-create-l2-message-table-up.sql
@@ -3,10 +3,11 @@ CREATE TABLE l2_message (
     unique_id INTEGER NOT NULL,
     batch_number INTEGER NOT NULL,
     batch_index INTEGER NOT NULL,
-    arb_block_number UNSIGNED BIG INT NOT NULL,
-    eth_block_number UNSIGNED BIG INT NOT NULL,
+    arb_block_number INTEGER INT NOT NULL,
+    eth_block_number INTEGER NOT NULL,
     timestamp DATETIME NOT NULL,
     call_value TEXT NOT NULL DEFAULT 0,
     data TEXT,
+    status VARCHAR(36) NOT NULL, -- SENT, EXECUTABLE
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
