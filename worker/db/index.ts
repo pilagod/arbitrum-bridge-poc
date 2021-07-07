abcd8a3ce79640423191eef8cacd37f9ad8d713e
pilagod <pilagooood@gmail.com>
2021-07-07 18:07:22 +0800

    add db instance

--- worker/db/index.ts
+++ worker/db/index.ts
@@ -1,6 +1,8 @@
 import path from "path";
 import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database(path.resolve(__dirname, "local.db"));
+const db = new (sqlite3.verbose().Database)(
+  path.resolve(__dirname, "local.db")
+);
 
 export default db;
