e36bf1401245d2cad98c74df0f28636b07e9894a
pilagod <pilagooood@gmail.com>
2021-07-09 19:35:55 +0800

    enable console log

--- worker/logger.ts
+++ worker/logger.ts
@@ -16,6 +16,7 @@ const logger = winston.createLogger({
     })
   ),
   transports: [
+    new winston.transports.Console(),
     new winston.transports.File({
       filename: path.resolve(__dirname, "worker.log"),
     }),
